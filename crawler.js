eb746d90869a2d8d4ac1d24a1d936c872ee92b30
Luis Eduardo <luisedumlg@gmail.com>
2019-11-01 19:30:25 -0300

    Crawler que captura todas os links de imagem

---
+++ crawler.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream =  fs.createWriteStream('image.csv');
+
+//Headers da tabela
+writeStream.write(`Numero,Link\n`)
+
+
+//Função Request para obter o html da página de sapatos.
+for( var index = 0; index < 536; index++){
+
+    var url = `https://www.arezzo.com.br/c/sapatos?q=%3Acreation-time&page=${index}`
+    request.get(url, (error,response,html) =>{
+        //Checando por erros
+        if( !error && response.statusCode==200){
+            const $ = cheerio.load(html);
+            var i = index*20 + 1;
+            $('.arz-storefront-product').each(function(){
+                var image = $(this).find('.arz-thumb').find('img').attr('data-original');
+                writeStream.write(`${i++},${image}\n`);
+            })
+        }
+    });
+}
